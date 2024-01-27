294c32e96b2379a6bd9166bd983ccc4346e8ecb7
jerzy szajner <jerzy.szajner@gmail.com>
2024-01-27 22:59:08 +0100

    feat: add external link functionality and style updates to CrisisChecker button
    - Implement external linking on button click in the CrisisChecker component.
    - Modify button hover state to change text from 'Sjekk!' to 'JA!' and background color to red.
    - Ensure consistent button width and center text alignment across different states.
    - Apply smooth transition effects for button color and text changes.

--- src/components/CrisisChecker/CrisisChecker.js
+++ src/components/CrisisChecker/CrisisChecker.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import styles from './CrisisChecker.module.scss';
 
 const CrisisChecker = () => {
+    const [buttonText, setButtonText] = useState('Sjekk!');
 
-  const checkForCrisis = () => {
+    const handleMouseEnter = () => setButtonText('JA!');
+    const handleMouseLeave = () => setButtonText('Sjekk!');
 
-    alert('Checking for crisis in Norway...');
-  };
+    const checkForCrisis = () => {
 
-  return (
-    <Container fluid className={styles.crisisChecker}>
-      <Row className="justify-content-md-center">
-        <Col md={12} className="text-center">
-          <h1 className={styles.title}>Er det krise i Norge nå?</h1>
-          <Button onClick={checkForCrisis} size="lg" className={styles.button} variant="secondary">
-            Sjekk
-          </Button>
-        </Col>
-      </Row>
-    </Container>
-  );
+        window.open('https://e24.no/naeringsliv/i/gEo909/fare-for-potetgull-krise-i-norge-tiltak-fra-myndighetene', '_blank');
+    };
+
+    return (
+        <Container fluid className={styles.crisisChecker}>
+            <Row className="justify-content-md-center">
+                <Col md={12} className="text-center">
+                    <h1 className={styles.title}>Er det krise i Norge nå?</h1>
+                    <Button
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        onClick={checkForCrisis}
+                        size="lg"
+                        className={styles.button}
+                        variant="secondary">
+                        {buttonText}
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default CrisisChecker;
